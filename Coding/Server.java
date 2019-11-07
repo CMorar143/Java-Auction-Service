ad7190acdddccc0bfc9b8e1c8548a3d8813d366e
CMorar143 <cian@morar.org>
2019-11-07 19:13:09 +0000

    server doesnt close after 1 connection

--- Coding/Server.java
+++ Coding/Server.java
@@ -1,23 +1,38 @@
 import java.net.*;
 import java.io.*;
 
-public class Server
-{
+public class Server implements Runnable
+{  
+	// Array of clients	
+	// private ChatServerThread clients[] = new ChatServerThread[50];
+	private ServerSocket server = null;
+	private Thread       thread = null;
+	private int clientCount = 0;
+
+	public void run()
+	{
+
+	}
+
 	public static void main(String[] args) throws IOException
 	{
-		ServerSocket ss = new ServerSocket(4999);
-		Socket s = ss.accept();
+		while(true)
+		{
+			ServerSocket ss = new ServerSocket(4999);
+			Socket s = ss.accept();
 
-		System.out.println("client connected");
+			System.out.println("client connected");
 
-		InputStreamReader in = new InputStreamReader(s.getInputStream());
-		BufferedReader bf = new BufferedReader(in);
+			InputStreamReader in = new InputStreamReader(s.getInputStream());
+			BufferedReader bf = new BufferedReader(in);
 
-		String str = bf.readLine();
-		System.out.println("client : " + str);
+			String str = bf.readLine();
+			System.out.println("client : " + str);
 
-		PrintWriter pr = new PrintWriter(s.getOutputStream());
-		pr.println("hello from SERVER");
-		pr.flush();
+			PrintWriter pr = new PrintWriter(s.getOutputStream());
+			pr.println("hello from SERVER");
+			pr.flush();
+			ss.close();
+		}
 	}
 }
